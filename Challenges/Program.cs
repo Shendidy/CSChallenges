53dc0997b47352509058220ff57cb9c3f1f3e6eb
Shendidy <sherif@shendidy.com>
2021-07-23 21:43:19 +0100

    test3 completed

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -2,13 +2,62 @@
 
 namespace Challenges
 {
+    public enum Side { None, Left, Right }
+
     class Program
     {
         static void Main(string[] args)
         {
-            // This repo has a separate branch for each challenge, and master will remain just a starting template for new challenges.
-            // To start a new challenge, just checkout a new branch with the name you want to give that challenge, then work there.
-            Console.WriteLine("Hello World!");
+            ChainLink left = new ChainLink();
+            ChainLink middle = new ChainLink();
+            ChainLink right = new ChainLink();
+            left.Append(middle);
+            middle.Append(right);
+            Console.WriteLine(left.LongerSide());
+        }
+    }
+
+    public class ChainLink
+    {
+        public ChainLink Left { get; private set; }
+        public ChainLink Right { get; private set; }
+
+        public void Append(ChainLink rightPart)
+        {
+            if (this.Right != null)
+                throw new InvalidOperationException("Link is already connected.");
+
+            this.Right = rightPart;
+            rightPart.Left = this;
+        }
+
+        public Side LongerSide()
+        {
+            int rightLinks = 0;
+            int leftLinks = 0;
+
+            ChainLink currentLink = this;
+
+
+            while (currentLink.Right != null)
+            {
+                rightLinks++;
+                currentLink = currentLink.Right;
+                if (currentLink == this) return (Side.None);
+            }
+
+            currentLink = this;
+
+            while (currentLink.Left != null)
+            {
+                leftLinks++;
+                currentLink = currentLink.Left;
+                if (currentLink == this) return (Side.None);
+            }
+
+            return leftLinks == rightLinks ? 
+                    Side.None :
+                    rightLinks > leftLinks ? Side.Right : Side.Left;
         }
     }
 }
