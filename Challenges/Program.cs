09f436a3db631942de9cd73fbb2fc677bf7f386a
Shendidy <sherif@shendidy.com>
2021-07-24 02:09:07 +0100

    final state with 3 out of 4 tests correct

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -6,9 +6,66 @@ namespace Challenges
     {
         static void Main(string[] args)
         {
-            // This repo has a separate branch for each challenge, and master will remain just a starting template for new challenges.
-            // To start a new challenge, just checkout a new branch with the name you want to give that challenge, then work there.
-            Console.WriteLine("Hello World!");
+            var chicken1 = new Chicken();
+            var egg = chicken1.Lay();
+            var childChicken = egg.Hatch();
+            Console.WriteLine(childChicken.GetType());
+
+            var chicken2 = new Chicken2();
+            var egg2 = chicken2.Lay();
+            var childChicken2 = egg2.Hatch();
+            Console.WriteLine(childChicken2.GetType());
+        }
+    }
+
+    public interface IBird
+    {
+        Egg Lay();
+    }
+
+    public class Chicken : IBird
+    {
+        public Chicken()
+        {
+        }
+
+        public Egg Lay()
+        {
+            return new Egg(() => this);
+        }
+    }
+
+    public class Chicken2 : IBird
+    {
+        public Chicken2()
+        {
+        }
+
+        public Egg Lay()
+        {
+            return new Egg(() => this);
+        }
+    }
+
+    public class Egg
+    {
+        private bool hatched = false;
+        private readonly Func<IBird> _createBird;
+
+        public Egg(Func<IBird> createBird)
+        {
+            _createBird = createBird;
+        }
+
+        public IBird Hatch()
+        {
+            if (!hatched)
+            {
+                hatched = true;
+                return _createBird();
+            }
+
+            throw new System.InvalidOperationException("This egg has hatched already!");
         }
     }
 }
