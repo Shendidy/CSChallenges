692c46a71d09512bceee448c00aa0e2d54215a36
Shendidy <sherif@shendidy.com>
2021-07-24 23:44:13 +0100

    final solution

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Challenges
 {
@@ -8,7 +9,23 @@ namespace Challenges
         {
             // This repo has a separate branch for each challenge, and master will remain just a starting template for new challenges.
             // To start a new challenge, just checkout a new branch with the name you want to give that challenge, then work there.
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Solution.Run(new int[] { 2, 4, 5, 4, 2, 5, 6, 8, 7, 7, 9, 8 }));
+        }
+    }
+
+    public class Solution
+    {
+        static public int Run(int[] student_list)
+        {
+            //
+            // Write your code below; return type and arguments should be according to the problem's requirements
+            //
+            Array.Sort(student_list);
+            for (int i = 1; i < student_list.Length; i++)
+                if (student_list[i - 1] != student_list[i]) return student_list[i - 1];
+                else i++;
+
+            return student_list.Last();
         }
     }
 }
--- FindTheSingleton.Tests/FindTheSingletonTests.cs
+++ FindTheSingleton.Tests/FindTheSingletonTests.cs
@@ -10,9 +10,13 @@ namespace FindTheSingleton.Tests
         }
 
         [Test]
-        public void Test1()
+        [TestCase(new int[] { 2, 5, 2, 3, 4, 4, 3 }, 5)]
+        [TestCase(new int[] { 2, 4, 5, 4, 2, 5, 6, 8, 7, 7, 9, 8, 6 }, 9)]
+        public void RunShouldReturnCorrectStudent(int[] students_list, int expected)
         {
-            Assert.Pass();
+            var actual = Challenges.Solution.Run(students_list);
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
